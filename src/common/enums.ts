701e33389ca2b8294495a45975e47c331817faf3
v_kangjianxuan <v_kangjianxuan@baidu.com>
2023-06-12 18:58:29 +0800

    页面调整和代码规范

---
+++ src/common/enums.ts
@@ -0,0 +1,121 @@
+/*
+* @file enums.ts
+* @author kangjianxuan
+*/
+
+// 货物类型
+export const GOODS_TYPE = [
+    {
+        label: '粮油及面粉类',
+        value: 1
+    },
+    {
+        label: "干货调料类",
+        value: 2
+    },
+    {
+        label: "蔬菜水果类",
+        value: 3
+    },
+    {
+        label: "肉类",
+        value: 4
+    },
+    {
+        label: "河粉",
+        value: 5
+    },
+    {
+        label: "牛奶类",
+        value: 6
+    },
+    {
+        label: "糕点类",
+        value: 7
+    },
+    {
+        label: "其它类",
+        value: 0
+    }
+];
+
+// 收入类型
+export const INCOME_TYPE = [
+    {
+        label: "收入",
+        value: 1
+    },
+    {
+        label: "早餐",
+        value: 2
+    },
+    {
+        label: "中餐",
+        value: 3
+    },
+    {
+        label: "加餐",
+        value: 3
+    },
+    {
+        label: "其它类",
+        value: 0
+    }
+];
+
+// 支出类型
+export const OUTCOME_TYPE = [
+    {
+        label:  "大米",
+        value: 1
+    },
+    {
+        label: "面粉",
+        value: 2
+    },
+    {
+        label: "食用油",
+        value: 3
+    },
+    {
+        label: "干货/调味品",
+        value: 4
+    },
+    {
+        label: "牛奶",
+        value: 5
+    },
+    {
+        label: "肉类",
+        value: 6
+    },
+    {
+        label: "鱼类",
+        value: 7
+    },
+    {
+        label: "面点类",
+        value: 8
+    },
+    {
+        label: "蔬菜",
+        value: 9
+    },
+    {
+        label: "燃气",
+        value: 10
+    },
+    {
+        label: "水电",
+        value: 11
+    },
+    {
+        label: "管理费",
+        value: 12
+    },
+    {
+        label: "其他",
+        value: 0
+    }
+]
+
---
+++ src/modules/store/views/out.vue
@@ -0,0 +1,301 @@
+<template>
+	<cl-crud ref='Crud'>
+		<cl-row>
+			<cl-add-btn />
+			<cl-multi-delete-btn />
+			<cl-flex1 />
+			<cl-adv-btn />
+			<cl-export-btn :columns='Table?.columns' :filename="'入库单'" />
+			<cl-refresh-btn />
+		</cl-row>
+		<cl-row>
+			<cl-table style='max-height: none;' ref='Table'></cl-table>
+		</cl-row>
+		<cl-row>
+			<cl-flex1 />
+			<!-- 分页 -->
+			<cl-pagination />
+		</cl-row>
+		<cl-upsert ref='Upsert' />
+		<cl-adv-search ref='AdvSearch' />
+	</cl-crud>
+</template>
+
+<script lang='ts' setup>
+import { h, onBeforeMount, ref, resolveComponent } from 'vue';
+import { useCrud, useTable, useUpsert, useAdvSearch } from '@cool-vue/crud';
+import { useCool } from '/@/cool';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { GOODS_TYPE } from '/@/common/enums';
+
+const { service } = useCool();
+
+const storeOutService = service.store.out;
+const formLabelProps = { labelWidth: '160px' };
+
+function fetchList() {
+	storeOutService.page({ size: 20, page: 1 }).then((res) => {
+		console.log(res, 'res');
+	});
+}
+
+onBeforeMount(() => {
+	fetchList();
+});
+// cl-crud
+const Crud = useCrud(
+	{
+		service: storeOutService,
+		permission: {
+			add: true,
+			delete: true,
+			update: true,
+			info: true,
+			edit: true
+		},
+		onDelete(selection, { next }) {
+			ElMessageBox.confirm(`已选择了${selection.length}项，是否继续？`, '提示', {
+				type: 'error'
+			})
+				.then(() => {
+					// 继续执行删除请求
+					next({
+						ids: selection.map((e) => e.id)
+					});
+				})
+				.catch(() => null);
+		}
+	},
+	(app) => {
+		app.refresh();
+	}
+);
+const Table = useTable({
+	columns: [
+		{
+			type: 'selection',
+			width: 60
+		},
+		{
+			label: 'ID',
+			prop: 'id'
+		},
+		{
+			label: '出库时间',
+			prop: 'outTime',
+		},
+		{
+			label: '领料人',
+			prop: 'material'
+		},
+		{
+			label: '内容',
+			prop: 'content',
+			children: [
+				{
+					label: '货物名称',
+					prop: 'goodsName',
+				},
+				{
+					label: '货物品类',
+					prop: 'goodsType',
+					formatter: row => GOODS_TYPE.find(item => item.value === row.goodsType)!.label
+				},
+				{
+					label: '数量',
+					prop: 'count'
+				},
+				{
+					label: '单位',
+					prop: 'unit'
+				},
+				{
+					label: '供应商',
+					prop: 'supplier'
+				},
+				{
+					label: '金额',
+					prop: 'price'
+				}
+			]
+		},
+		{
+			label: '状态',
+			prop: 'status'
+		},
+		{
+			label: '附件',
+			prop: 'attachment'
+		},
+		{
+			type: 'op',
+			width: 250,
+			buttons: ['info', 'edit', 'delete']
+		}
+	]
+});
+const Upsert = useUpsert({
+	items: [
+		{
+			label: '出库时间',
+			prop: 'outTime',
+			props: formLabelProps,
+			component: {
+				name: 'el-date-picker'
+			}
+		},
+		{
+			label: '领料人',
+			prop: 'material',
+			props: formLabelProps,
+			component: {
+				name: 'el-input'
+			}
+		},
+		{
+			label: '货物名称',
+			prop: 'goodsName',
+			props: formLabelProps,
+			component: {
+				name: 'el-input'
+			}
+		},
+		{
+			label: '货物品类',
+			prop: 'goodsType',
+			props: formLabelProps,
+			component: {
+				name: 'el-select',
+				options: GOODS_TYPE
+			}
+		},
+		{
+			label: '数量',
+			prop: 'count',
+			props: formLabelProps,
+			component: {
+				name: 'el-input-number',
+			}
+		},
+		{
+			label: '单位',
+			prop: 'unit',
+			props: formLabelProps,
+			component: {
+				name: 'el-input'
+			}
+		},
+		{
+			label: '供应商',
+			prop: 'supplier',
+			props: formLabelProps,
+			component: {
+				name: 'el-input'
+			}
+		},
+		{
+			label: '金额',
+			prop: 'price',
+			props: formLabelProps,
+			component: {
+				name: 'el-input-number',
+			}
+		},
+		{
+			label: '状态',
+			prop: 'status',
+			props: formLabelProps,
+			component: {
+				name: 'el-input',
+			}
+		},
+		{
+			label: '附件',
+			prop: 'attachment',
+			props: formLabelProps,
+			component: {
+				name: 'cl-upload',
+				type: ['file', 'image'],
+				props: {
+					text: "上传附件",
+					multiple: true
+				}
+			}
+		}
+	],
+
+	dialog: {
+		width: '580px'
+	},
+
+	// // 插件
+	// plugins: [
+	// 	// 自动聚焦
+	// 	setFocus('name')
+	// ],
+
+	// 详情钩子
+	onInfo(data, { next, done }) {
+		// 继续请求 info 接口
+		// next({
+		// 	id: data.id
+		// });
+
+		// 直接取列表的数据返回
+		done(data);
+	},
+
+	// 提交钩子
+	onSubmit(data, { next, close, done }) {
+		console.log('onSubmit', data);
+		// 继续请求 update/add 接口
+		next(data);
+	},
+
+	// 打开后
+	onOpened(data) {
+		if (Upsert.value?.mode != 'info') {
+			ElMessage.success('编辑中');
+		}
+	},
+
+	// 关闭钩子
+	onClose(action, done) {
+		if (Upsert.value?.mode == 'update') {
+			if (action == 'close') {
+				return ElMessageBox.confirm('还没填完，确定关闭不？', '提示', {
+					type: 'warning'
+				})
+					.then(() => {
+						done();
+						ElMessage.info('好吧');
+					})
+					.catch(() => {
+						ElMessage.success('请继续编辑');
+					});
+			}
+		}
+
+		done();
+	}
+});
+
+const AdvSearch = useAdvSearch({
+	items: [
+		{
+			label: 'ID',
+			prop: 'id',
+			component: {
+				name: 'el-input'
+			}
+		},
+		{
+			label: '时间',
+			prop: 'sampleDate',
+			component: {
+				name: 'el-time-picker',
+			}
+		}
+	]
+});
+</script>
